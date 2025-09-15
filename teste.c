c3627ce5952743880e40fa677e08f5debc5002fc
Daniel Marques <danielmateus631@gmail.com>
2025-09-15 08:03:03 +0100

    Remove function done

--- teste.c
+++ teste.c
@@ -2,6 +2,7 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 sqlite3 *db;
 char *err_msg = 0;
@@ -11,6 +12,10 @@ void botao_inserir_done_clicked(GtkButton *button, gpointer user_data);
 void botao_inserir_inserir_clicked(GtkButton *button, gpointer user_data);
 void botao_inserir_cancelar_clicked(GtkButton *button, gpointer user_data);
 void botao_inserir_clicked(GtkButton *button, gpointer user_data);
+void botao_janela_remover_done_clicked(GtkButton *button, gpointer user_data);
+void botao_janela_remover_remover_clicked(GtkButton *button, gpointer user_data);
+void botao_janela_remover_cancelar_clicked(GtkButton *button, gpointer user_data);
+void botao_remover_clicked(GtkButton *button, gpointer user_data);
 
 int main(int argc, char *argv[]){
     
@@ -37,6 +42,7 @@ int main(int argc, char *argv[]){
     GtkWidget *botao_listar = GTK_WIDGET(gtk_builder_get_object(builder, "botao_listar"));
 
     g_signal_connect(botao_inserir, "clicked", G_CALLBACK(botao_inserir_clicked), NULL);
+    g_signal_connect(botao_remover, "clicked", G_CALLBACK(botao_remover_clicked), NULL);
     gtk_builder_connect_signals(builder, NULL);
 
     gtk_widget_show_all(janela_principal);
@@ -45,6 +51,16 @@ int main(int argc, char *argv[]){
 
     sqlite3_close(db);
     return 0;
+}
+
+//Função inserir (inicio).
+bool is_right(int idade, int telefone){
+    int count = 0;
+    while (telefone > 0){
+        telefone = telefone / 10;
+        count ++;
+    }
+    return (idade > 0 && idade < 120) && (count <= 15); 
 }
 
 void botao_inserir_erro_clicked(GtkButton *button, gpointer user_data){
@@ -74,17 +90,19 @@ void botao_inserir_inserir_clicked(GtkButton *button, gpointer user_data){
     // Converte as entradas para inteiro.
     int idade_int = atoi(idade);
     int telefone_int = atoi(telefone);
-
-    char sql[200];
-    sprintf(sql, "INSERT INTO clientes (Nome, Idade, Telefone) VALUES ('%s', %d, %d)", nome, idade_int, telefone_int);
-    if(sqlite3_exec(db, sql, 0, 0, &err_msg) != SQLITE_OK){
-        gtk_widget_show_all(janela_inserir_erro);
-        g_signal_connect(botao_inserir_done, "clicked", G_CALLBACK(botao_inserir_erro_clicked), user_data);
-        printf("Erro: %s", &err_msg);
-        sqlite3_free(err_msg);
-    }else{
+    bool check = is_right(idade_int, telefone_int);
+    if (check){
+        char sql[200];
+        sprintf(sql, "INSERT INTO clientes (Nome, Idade, Telefone) VALUES ('%s', %d, %d)", nome, idade_int, telefone_int);
+        if(sqlite3_exec(db, sql, 0, 0, &err_msg) != SQLITE_OK){
+            printf("Erro: %s", &err_msg);
+            sqlite3_free(err_msg);
+        }
         gtk_widget_show_all(janela_inserir_done);
         g_signal_connect(botao_inserir_done, "clicked", G_CALLBACK(botao_inserir_done_clicked), user_data);
+    }else{
+        gtk_widget_show_all(janela_inserir_erro);
+        g_signal_connect(botao_inserir_erro, "clicked", G_CALLBACK(botao_inserir_erro_clicked), user_data);
     }
     gtk_widget_destroy(janela_inserir);
 }
@@ -104,4 +122,66 @@ void botao_inserir_clicked(GtkButton *button, gpointer user_data){
     g_signal_connect(botao_inserir_inserir, "clicked", G_CALLBACK(botao_inserir_inserir_clicked), builder);
     g_signal_connect(botao_inserir_cancelar, "clicked", G_CALLBACK(botao_inserir_cancelar_clicked), builder);
     
+}
+//Função inserir (Fim)
+
+// Função remover (Inicio)
+void botao_janela_remover_done_clicked(GtkButton *button, gpointer user_data){
+     GtkWidget *janela_remover_done = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover_done"));
+     gtk_widget_destroy(janela_remover_done);  
+}
+void botao_janela_remover_erro_clicked(GtkButton *button, gpointer user_data){
+    GtkWidget *janela_remover_erro = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover_erro"));
+     gtk_widget_destroy(janela_remover_erro);  
+}
+void botao_janela_remover_cancelar_clicked(GtkButton *button, gpointer user_data){
+    GtkWidget *janela_remover = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover"));
+    gtk_widget_destroy(janela_remover); 
+}
+
+void botao_janela_remover_remover_clicked(GtkButton *button, gpointer user_data){
+    GtkWidget *janela_remover = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover"));
+    GtkWidget *entry_janela_remover = GTK_WIDGET(gtk_builder_get_object(user_data, "entry_janela_remover"));
+    GtkWidget *escolha_janela_remover = GTK_WIDGET(gtk_builder_get_object(user_data, "escolha_janela_remover"));
+    GtkWidget *janela_remover_done = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover_done"));
+    GtkWidget *janela_remover_erro = GTK_WIDGET(gtk_builder_get_object(user_data, "janela_remover_erro"));
+    GtkWidget *botao_janela_remover_done = GTK_WIDGET(gtk_builder_get_object(user_data, "botao_janela_remover_done"));
+    GtkWidget *botao_janela_remover_erro = GTK_WIDGET(gtk_builder_get_object(user_data, "botao_janela_remover_erro"));
+
+    const gchar *info = gtk_entry_get_text(GTK_ENTRY(entry_janela_remover));
+    const gchar *escolha = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(escolha_janela_remover));
+    int info_int = atoi(info);
+    char sql[200];
+    if(g_strcmp0(escolha, "ID") == 0){
+        sprintf(sql, "DELETE FROM clientes WHERE Id = %d", info_int);
+    }else if(g_strcmp0(escolha, "Telefone") == 0){
+        sprintf(sql, "DELETE FROM clientes WHERE Telefone = %d", info_int);
+    }
+    
+    int rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    int changes = sqlite3_changes(db);
+    if(rc != SQLITE_OK){
+        printf("Erro: %s\n", &err_msg);
+        sqlite3_free(err_msg);
+    }
+    if(changes > 0){
+        gtk_widget_show_all(janela_remover_done);
+        g_signal_connect(botao_janela_remover_done, "clicked", G_CALLBACK(botao_janela_remover_done_clicked), user_data);
+    }else{
+        gtk_widget_show_all(janela_remover_erro);
+        g_signal_connect(botao_janela_remover_erro, "clicked", G_CALLBACK(botao_janela_remover_erro_clicked), user_data);
+    }
+
+    gtk_widget_destroy(janela_remover);    
+    
+}
+
+void botao_remover_clicked(GtkButton *button, gpointer user_data){
+    GtkBuilder *builder = gtk_builder_new_from_file("interface.ui");
+    GtkWidget *janela_remover = GTK_WIDGET(gtk_builder_get_object(builder, "janela_remover"));
+    GtkWidget *botao_janela_remover_remover = GTK_WIDGET(gtk_builder_get_object(builder, "botao_janela_remover_remover"));
+    GtkWidget *botao_janela_remover_cancelar = GTK_WIDGET(gtk_builder_get_object(builder, "botao_janela_remover_cancelar"));
+    gtk_widget_show_all(janela_remover);
+    g_signal_connect(botao_janela_remover_remover, "clicked", G_CALLBACK(botao_janela_remover_remover_clicked), builder);
+    g_signal_connect(botao_janela_remover_cancelar, "clicked", G_CALLBACK(botao_janela_remover_cancelar_clicked), builder);
 }
